32b8bac30a417638c85d316793e7e423332079a4
stevnickOVO <stevnick35@gmail.com>
2022-03-30 16:29:59 +0800

    DarkSoul3-30
    Add-Terrain
    Add-PlayerControllor
                   -Move
                   -Attack
    Add-PlayerAnimator

--- DarkSoul/Assets/script/Player/PlayerControllor.cs
+++ DarkSoul/Assets/script/Player/PlayerControllor.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class PlayerControllor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    [SerializeField] private float dashColdDown;
+    [SerializeField] private float dashSpeed;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject groundCheck;
+    int attackCount;
+    float gravity = -18f;
+    Vector3 gravityVectory=Vector3.zero;
+    CharacterController characterController;
+    Animator animator;
+    private void Awake()
     {
-        
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        CharacterMovetion();
+        Attack();
+
+        if (Input.GetButtonDown("Jump")) StartCoroutine(PlayerDash());
+    }
+    public void CharacterMovetion()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, 0, vertical).normalized;
+        var move = dir * speed * Time.deltaTime;
+
+
+        if (!Physics.CheckSphere(groundCheck.transform.position, 0.1f, groundLayer))
+        {
+            gravityVectory.y += gravity * Time.deltaTime;
+        }
+        else gravityVectory = Vector3.zero;
+
+
+        characterController.Move(gravityVectory*Time.deltaTime);
+        characterController.Move(move);
+
+        var PlayerPoint = Camera.main.WorldToScreenPoint(transform.position);
+        var point = Input.mousePosition - PlayerPoint;
+        float angle = Mathf.Atan2(point.x, point.y) * Mathf.Rad2Deg;
+
+        transform.eulerAngles=new Vector3(transform.eulerAngles.x,angle,transform.eulerAngles.z);
+    }
+    public void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (attackCount <= 0)
+            {
+                animator.SetBool("Attack1", true);
+                attackCount++;
+            }
+            else if (attackCount <= 1)
+            {
+                animator.SetBool("Attack2", true);
+                attackCount=0;
+            }
+        }
+    }
+    IEnumerator  PlayerDash()
+    {
+        float time = Time.time;
+        
+        while (Time.time < time + dashColdDown) { 
+            Vector3 forword = transform.forward;
+            forword.y = 0;
+
+            characterController.Move(forword*dashSpeed*Time.deltaTime);
+            yield return null;
+        }
+        
         
     }
 }
